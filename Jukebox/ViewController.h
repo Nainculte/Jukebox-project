b0d6e2e3d24fba4703b5f81c3e0ac39abccef9ec
Olivier Dumenil <olive.dumenil@gmail.com>
2014-11-29 17:58:37 +0100

    Base project
    Ready to go

---
+++ Jukebox/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Jukebox
+//
+//  Created by Nainculte on 29/11/2014.
+//  Copyright (c) 2014 Nainculte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
